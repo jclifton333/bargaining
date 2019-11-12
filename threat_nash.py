56602328098b1b9599f354774d36b9915a1ec590
jesse <jclifton333@gmail.com>
2019-11-12 14:04:46 -0500

    plot boy

--- threat_nash.py
+++ threat_nash.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import nashpy as nash
+
 
 
 def threat_game_nash(p_commit_1, p_commit_2, cost_1, cost_2):
@@ -33,16 +36,16 @@ def nash_bargaining_solution(p_commit_1, p_commit_2, disagreement_points):
   :return:
   """
   def nash_welfare(q):
-    player_1_nash_welfare = np.log(q + p_commit_2)*(1 - p_commit_1) + \
-                            np.log(q - disagreement_points[0])*p_commit_1
-    player_2_nash_welfare = np.log(1 - q + p_commit_1)*(1 - p_commit_2) + \
-                            np.log(1 - q - disagreement_points[1])*p_commit_2
+    player_1_nash_welfare = np.log(q - (1-q) + p_commit_2)*(1 - p_commit_1) + \
+                            np.log(q - (1-q) - disagreement_points[0])*p_commit_1
+    player_2_nash_welfare = np.log(1 - q - q + p_commit_1)*(1 - p_commit_2) + \
+                            np.log(1 - q - q - disagreement_points[1])*p_commit_2
     return player_1_nash_welfare + player_2_nash_welfare
 
   # Brute force search for NBS
   pstar = None
   max_welfare = -float('inf')
-  for p_ in np.linspace(0.01, 0.99, 99):
+  for p_ in np.linspace(0.01, 0.99, 10000):
     p_welfare = nash_welfare(p_)
     if p_welfare > max_welfare:
       pstar = p_
@@ -57,8 +60,26 @@ def threat_game_bargaining(p_commit_1, p_commit_2, cost_1, cost_2):
 
 
 if __name__ == "__main__":
-  # for p in np.linspace(0.1, 0.9, 9):
-  for p in np.linspace(0.1, 0.9, 9):
-    # threat_game_nash(p, 0.4, -5, -5)
-    print(threat_game_bargaining(p, 0.4, -10, -10))
+  solutions = []
+  probs = np.linspace(0.0, 1.0, 100)
+  for p in probs:
+    s = threat_game_bargaining(p, 0.5, -5, -5)
+    solutions.append(s)
+  sns.set_style("darkgrid")
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.set_title("Nash bargaining allocation as a function of threat credibility", fontsize=25)
+  ax.set_xlabel('Prior that Player 1 has commitment device', fontsize=20)
+  ax.set_ylabel('Bargaining proportion for Player 1', fontsize=20)
+  for tick in ax.xaxis.get_major_ticks():
+    tick.label.set_fontsize(14)
+  for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(14)
+  ax.plot(probs, solutions)
+  plt.show()
+  # plt.plot(probs, solutions)
+  # plt.xlabel('Prior that Player 1 has commitment device')
+  # plt.ylabel('Bargaining proportion for Player 1')
+  # plt.title("Nash bargaining allocation as a function of threat credibility")
+  # plt.show()
 
