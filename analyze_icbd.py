928f1da6e1ec765ed28cf821a30db8a1c8e7e4e0
Jesse Clifton <jesseclifton@jesses-imac-pro.mynet>
2020-07-19 23:55:48 +0100

    naive classifiers

--- analyze_icbd.py
+++ analyze_icbd.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
 
 if __name__ == "__main__":
   df = pd.read_csv('icb2v13.csv')
@@ -17,9 +18,28 @@ if __name__ == "__main__":
   # independent variables: cols 64-86
   # dependent var: 44
 
-  keep = ['outcom', 'territ', 'allycap', 'issue', 'pethin', 'powdis',
-          'actloc', 'geog', 'cractloc']
+
+  ind = ['territ', 'allycap', 'issue', 'pethin', 'powdis', 'actloc', 'geog', 'cractloc']
+  dep = ['outcom']
+  keep = ind + dep
   df = df.loc[df.sevvio == 4]
-  df = df.loc[df.outcom in [1, 4]]
-  df = df.loc[df.gravity in [1, 3, 4, 5, 6]]
-  df.outcome = (df.outcome == 1)
+  df = df.loc[df.outcom.isin([1, 4])]
+  df = df.loc[df.gravty.isin([1, 3, 4, 5, 6])]
+  df.outcome = (df.outcom == 1)
+  df = df.loc[:, keep]
+  df.dropna(axis='rows', inplace=True)
+
+  X = df.loc[:, ind]
+  y = df.loc[:, dep]
+
+  lm = LogisticRegression()
+  nb = BernoulliNB()
+  lm.fit(X, y)
+  nb.fit(X, y)
+
+  phat_lm = lm.predict_proba(X)[:, -1]
+  phat_nb = nb.predict_proba(X)[:, -1]
+
+
+
+
