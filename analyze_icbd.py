3f45fca4a90bbb518ee8c967d4283924ac7acc1a
Jesse Clifton <jesseclifton@jesses-imac-pro.mynet>
2020-07-20 00:35:20 +0100

    (wip) implementing bayesian logistic reg

--- analyze_icbd.py
+++ analyze_icbd.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import BernoulliNB
+import pymc3 as pm
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
   df = pd.read_csv('icb2v13.csv')
@@ -25,20 +27,29 @@ if __name__ == "__main__":
   df = df.loc[df.sevvio == 4]
   df = df.loc[df.outcom.isin([1, 4])]
   df = df.loc[df.gravty.isin([1, 3, 4, 5, 6])]
-  df.outcome = (df.outcom == 1)
+  df.loc[df.outcom == 4, 'outcom'] = 1
+  df.loc[df.outcom == 1, 'outcom'] = 0
   df = df.loc[:, keep]
   df.dropna(axis='rows', inplace=True)
 
   X = df.loc[:, ind]
   y = df.loc[:, dep]
 
-  lm = LogisticRegression()
-  nb = BernoulliNB()
-  lm.fit(X, y)
-  nb.fit(X, y)
+  priors_1 = {'powdis': pm.Normal.dist(mu=0., sigma=1.)}
+  priors_2 = {'powdis': pm.Normal.dist(mu=0., sigma=10.)}
 
-  phat_lm = lm.predict_proba(X)[:, -1]
-  phat_nb = nb.predict_proba(X)[:, -1]
+  # ToDo: posterior probs of winning
+  with pm.Model() as model:
+    pm.glm.GLM.from_formula('outcom ~ powdis', df, family=pm.glm.families.Binomial(),
+                            priors=priors_1)
+    fitted = pm.fit(method='fullrank_advi')
+    trace_big_1 = fitted.sample(2000)
+
+  with pm.Model() as model:
+    pm.glm.GLM.from_formula('outcom ~ powdis', df, family=pm.glm.families.Binomial(),
+                            priors=priors_2)
+    fitted = pm.fit(method='fullrank_advi')
+    trace_big_2 = fitted.sample(2000)
 
 
 
