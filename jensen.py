a62a517ce7b6c5d4d6276150f756c6d86c825f27
Jesse Clifton <jesseclifton@192.168.1.126>
2021-08-09 17:53:01 +0100

    plot jensen

--- jensen.py
+++ jensen.py
@@ -6,15 +6,33 @@ import seaborn as sns
 
 if __name__ == "__main__":
   expected_effect = -1
-  noise_multipliers = np.logspace(-1, 1, 10)
-  base_probabilities = np.logspace(-5, -2, 10)
-  results = np.zeros((10, 10))
+  noise_multipliers = np.logspace(-1, 1, 100)
+  base_probabilities = np.logspace(-5, -2, 100)
+  results = np.zeros((100, 100))
   for noise_ix, noise_multiplier in enumerate(noise_multipliers):
     for prob_ix, base_prob in enumerate(base_probabilities):
       base_logit = logit(base_prob)
       noisy_logits = base_logit + np.random.normal(loc=expected_effect, scale=noise_multiplier, size=10000)
       expected_improvement = base_prob - np.mean(expit(noisy_logits))
-      results[9-noise_ix, prob_ix] = expected_improvement
-  sns.heatmap(results)
-  # plt.show()
+      results[99-noise_ix, prob_ix] = expected_improvement
+
+  xticklabels = []
+  yticklabels = []
+  log_base_probs = np.log10(base_probabilities).round(1)
+  log_noise = np.log10(noise_multipliers).round(1)
+  np.flip(log_noise)
+  sign_log_abs = np.sign(results) * np.log10(np.abs(results))
+  for i in range(100):
+    if i % 10 == 0:
+      xticklabels.append(str(log_base_probs[i]))
+      yticklabels.append(str(log_noise[i]))
+    else:
+      xticklabels.append('')
+      yticklabels.append('')
+  ax = sns.heatmap(results, xticklabels=xticklabels, yticklabels=yticklabels)
+  ax.set_xlabel('log default probability')
+  ax.set_ylabel('log noise standard dev')
+  # ax = sns.heatmap(results)
+  # ax.axis([base_probabilities.min(), base_probabilities.max(), noise_multipliers.max(), noise_multipliers.min()])
+  plt.show()
 
