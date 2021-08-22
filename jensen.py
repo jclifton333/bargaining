41e68f3e2a7310bf76a9b6c6c3b288dd3cba86fd
Jesse Clifton <jclifton333@gmail.com>
2021-08-23 00:07:24 +0100

    psychometric stuff

--- jensen.py
+++ jensen.py
@@ -6,41 +6,20 @@ import seaborn as sns
 
 if __name__ == "__main__":
   expected_effect = -0.1
-  # noise_multipliers = np.logspace(-2, 0, 100)
   noise_multipliers = np.linspace(0.01, 1, 100)
-  base_probabilities = np.logspace(-5, -2, 100)
   results = np.zeros((100, 100))
+  base_prob = 10**-5
   for noise_ix, noise_multiplier in enumerate(noise_multipliers):
-    for prob_ix, base_prob in enumerate(base_probabilities):
+    for replicate_ix in range(100):
       base_logit = logit(base_prob)
       noisy_logits = base_logit + np.random.normal(loc=expected_effect, scale=noise_multiplier, size=10000)
       expected_improvement = np.mean(expit(noisy_logits)) / base_prob
-      results[99-noise_ix, prob_ix] = expected_improvement
+      results[99-noise_ix, replicate_ix] = expected_improvement
 
-  xticklabels = []
-  yticklabels = []
-  log_base_probs = np.log10(base_probabilities).round(1)
   log_noise = np.log10(noise_multipliers / np.abs(expected_effect)).round(1)
   log_noise = log_noise[::-1]
   noise = (noise_multipliers / np.abs(expected_effect)).round(1)
   noise = noise[::-1]
-  # sign_log_abs = np.log10(np.abs(results))
-  for i in range(100):
-    if i % 10 == 0:
-      xticklabels.append(str(log_base_probs[i]))
-      # yticklabels.append(str(log_noise[i]))
-      yticklabels.append(str(noise[i]))
-    else:
-      xticklabels.append('')
-      yticklabels.append('')
-  # ax = sns.heatmap(results, xticklabels=xticklabels, yticklabels=yticklabels, center=1, cmap='PiYG')
-  # ax.set_xlabel('log default probability')
-  # # ax.set_ylabel(r'$\log( \sigma / \delta$ )')
-  # ax.set_ylabel(r'$\sigma / \delta$')
-  # ax.collections[0].colorbar.set_label('expected intervention prob / base prob')
-  # # ax = sns.heatmap(results)
-  # # ax.axis([base_probabilities.min(), base_probabilities.max(), noise_multipliers.max(), noise_multipliers.min()])
-  # plt.show()
   ratios = results.mean(axis=1)
   plt.plot(noise, ratios)
   plt.hlines(1, noise.min(), noise.max(), linestyles='dashed')
---
+++ psychometrc.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pdb
+
+
+def generate_tc_cross_pd_matrix(social_dilemma_weight=1., tc_bonus=1., tc_byproduct=0.):
+  tc_equal_1 = np.array([[0., 0.], [0., 1.]]) * social_dilemma_weight
+  tc_equal_2 = np.array([[0., 0.], [0., 1.]]) * social_dilemma_weight
+
+  tc_less_1 = tc_byproduct * np.ones((2, 2))
+  tc_less_2 = tc_byproduct * np.ones((2, 2))
+
+  tc_greater_1 = tc_bonus * np.ones((2, 2))
+  tc_greater_2 = tc_bonus * np.ones((2, 2))
+
+  u1 = np.column_stack((
+    np.vstack((tc_equal_1 + tc_greater_1 + tc_byproduct, tc_less_1)),
+    np.vstack((tc_greater_1, tc_equal_1))
+  ))
+
+  u2 = np.column_stack((
+    np.vstack((tc_equal_2 + tc_greater_2 + tc_byproduct, tc_greater_2)),
+    np.vstack((tc_less_2, tc_equal_2))
+  ))
+
+  return u1, u2
+
+
+def get_type1_policy_payoffs(u1, u2, pi1, pi2):
+  # Policies parameterized by (prob high task competence, prob cooperate)
+  phigh1, pcoop1 = pi1
+  phigh2, pcoop2 = pi2
+  plow1 = 1 - phigh1
+  plow2 = 1 - phigh2
+  pdefect1 = 1 - pcoop1
+  pdefect2 = 1 - pcoop2
+
+  s1 = np.array([phigh1 * pdefect1, phigh1 * pcoop1, plow1 * pdefect1, plow1 * pcoop1])
+  s2 = np.array([phigh2 * pdefect2, phigh2 * pcoop2, plow2 * pdefect2, plow2 * pcoop2])
+
+  ev1 = np.dot(s1.T, np.dot(u1, s2))
+  ev2 = np.dot(s2.T, np.dot(u2, s1))
+
+  return ev1, ev2
+
+
+if __name__ == "__main__":
+  np.random.seed(3)
+
+  # Generate policies
+  pi1_list = np.random.uniform(low=0, high=1, size=(12, 2))
+  pi2_list = np.random.uniform(low=0, high=1, size=(12, 2))
+
+  # Two clusters: small tc and large cc component; large tc and small cc component
+  num_envs_to_generate = 6
+  u1_list = []
+  u2_list = []
+
+  for _ in range(num_envs_to_generate // 2):
+    social_dilemma_weight = np.random.gamma(shape=1, scale=1)
+    tc_bonus = np.random.gamma(shape=0.01, scale=1)
+    tc_byproduct = np.random.gamma(shape=0.01, scale=1)
+    u1, u2 = generate_tc_cross_pd_matrix(social_dilemma_weight=social_dilemma_weight, tc_bonus=tc_bonus,
+                                         tc_byproduct=tc_byproduct)
+    u1_list.append(u1)
+    u2_list.append(u2)
+
+  for _ in range(num_envs_to_generate // 2):
+    social_dilemma_weight = np.random.gamma(shape=0.01, scale=1)
+    tc_bonus = np.random.gamma(shape=1, scale=1)
+    tc_byproduct = np.random.gamma(shape=1, scale=1)
+    u1, u2 = generate_tc_cross_pd_matrix(social_dilemma_weight=social_dilemma_weight, tc_bonus=tc_bonus,
+                                         tc_byproduct=tc_byproduct)
+    u1_list.append(u1)
+    u2_list.append(u2)
+
+  # Construct feature matrix
+  X = np.zeros((0, 2*num_envs_to_generate))
+  for pi1 in pi1_list:
+    for pi2 in pi2_list:
+      x = np.zeros(0)
+      for u1, u2 in zip(u1_list, u2_list):
+        ev1, ev2 = get_type1_policy_payoffs(u1, u2, pi1, pi2)
+        x = np.hstack((x, [ev1, ev2]))
+      X = np.vstack((X, x))
+
+
+
+
